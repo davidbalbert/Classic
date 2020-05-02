dd153d242ac3309494f25149b3311fcfae7d1003
David Albert <davidbalbert@gmail.com>
2020-05-02 19:13:48 -0400

    Initial Commit

---
+++ Classic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Classic
+//
+//  Created by David Albert on 5/2/20.
+//  Copyright © 2020 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Classic/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Classic
+//
+//  Created by David Albert on 5/2/20.
+//  Copyright © 2020 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
