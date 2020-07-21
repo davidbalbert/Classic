2f6ee9992197d8ce955db5f3e6b39c8d3ad32b7c
David Albert <davidbalbert@gmail.com>
2020-07-21 09:29:16 -0400

    Embed outline view in lower split

---
+++ Classic/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  Classic
+//
+//  Created by David Albert on 7/21/20.
+//  Copyright © 2020 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+class RegisterViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
