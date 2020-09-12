e788c8062e9fd7d87025dd118202dbd6815f62cd
David Albert <davidbalbert@gmail.com>
2020-09-12 16:38:12 -0400

    lsr reg

--- M68KTests/InstructionTests/LsrTests.swift
+++ M68KTests/InstructionTests/LsrTests.swift
@@ -110,4 +110,141 @@ class LsrTests: XCTestCase {
         XCTAssertEqual(m.cpu.ccr, [.x, .c, .z])
     }
 
+    
+    
+    
+    
+    
+    
+    func testLsrByteReg() throws {
+        m.cpu.d0 = 0b1_1010_1010
+        m.cpu.d1 = 3
+        m.cpu.ccr = [.n, .z, .v, .x, .c]
+        
+        m.cpu.execute(.lsr(.b, .r(.d1), .d0), length: 0)
+        
+        XCTAssertEqual(m.cpu.d0, 0b1_0001_0101)
+        XCTAssertEqual(m.cpu.ccr, [])
+    }
+    
+    func testLsrByteRegCarry() throws {
+        m.cpu.d0 = 0b1010_1010
+        m.cpu.d1 = 2
+        m.cpu.ccr = []
+        
+        m.cpu.execute(.lsr(.b, .r(.d1), .d0), length: 0)
+        
+        XCTAssertEqual(m.cpu.d0, 0b0010_1010)
+        XCTAssertEqual(m.cpu.ccr, [.x, .c])
+    }
+    
+    func testLsrByteRegZero() throws {
+        m.cpu.d0 = 0b1010_1010
+        m.cpu.d1 = 8
+        m.cpu.ccr = []
+        
+        m.cpu.execute(.lsr(.b, .r(.d1), .d0), length: 0)
+        
+        XCTAssertEqual(m.cpu.d0, 0)
+        XCTAssertEqual(m.cpu.ccr, [.x, .c, .z])
+    }
+    
+    func testLsrByteRegShiftCountZero() throws {
+        m.cpu.d0 = 0b0101_0101
+        m.cpu.d1 = 0
+        m.cpu.ccr = [.x, .c]
+        
+        m.cpu.execute(.lsr(.b, .r(.d1), .d0), length: 0)
+
+        XCTAssertEqual(m.cpu.d0, 0b0101_0101)
+        XCTAssertEqual(m.cpu.ccr, [.x])
+    }
+    
+    func testLsrWordReg() throws {
+        m.cpu.d0 = 0b1_1010_1010_1010_1010
+        m.cpu.d1 = 3
+        m.cpu.ccr = [.n, .z, .v, .x, .c]
+        
+        m.cpu.execute(.lsr(.w, .r(.d1), .d0), length: 0)
+        
+        XCTAssertEqual(m.cpu.d0, 0b1_0001_0101_0101_0101)
+        XCTAssertEqual(m.cpu.ccr, [])
+    }
+    
+    func testLsrWordRegCarry() throws {
+        m.cpu.d0 = 0b1010_1010_1010_1010
+        m.cpu.d1 = 2
+        m.cpu.ccr = []
+        
+        m.cpu.execute(.lsr(.w, .r(.d1), .d0), length: 0)
+        
+        XCTAssertEqual(m.cpu.d0, 0b0010_1010_1010_1010)
+        XCTAssertEqual(m.cpu.ccr, [.x, .c])
+    }
+    
+    func testLsrWordRegZero() throws {
+        m.cpu.d0 = 0b0000_0000_1010_1010
+        m.cpu.d1 = 8
+        m.cpu.ccr = []
+        
+        m.cpu.execute(.lsr(.w, .r(.d1), .d0), length: 0)
+        
+        XCTAssertEqual(m.cpu.d0, 0)
+        XCTAssertEqual(m.cpu.ccr, [.x, .c, .z])
+    }
+    
+    func testLsrWordRegShiftCountZero() throws {
+        m.cpu.d0 = 0b0110_1100_0101_0101
+        m.cpu.d1 = 0
+        m.cpu.ccr = [.x, .c]
+        
+        m.cpu.execute(.lsr(.w, .r(.d1), .d0), length: 0)
+
+        XCTAssertEqual(m.cpu.d0, 0b0110_1100_0101_0101)
+        XCTAssertEqual(m.cpu.ccr, [.x])
+    }
+
+    func testLsrLongReg() throws {
+        m.cpu.d0 = 0b1010_1010_1010_1010_1010_1010_1010_1010
+        m.cpu.d1 = 3
+        m.cpu.ccr = [.n, .z, .v, .x, .c]
+        
+        m.cpu.execute(.lsr(.l, .r(.d1), .d0), length: 0)
+        
+        XCTAssertEqual(m.cpu.d0, 0b0001_0101_0101_0101_0101_0101_0101_0101)
+        XCTAssertEqual(m.cpu.ccr, [])
+    }
+    
+    func testLsrLongRegCarry() throws {
+        m.cpu.d0 = 0b1010_1010_1010_1010_1010_1010_1010_1010
+        m.cpu.d1 = 2
+        m.cpu.ccr = []
+        
+        m.cpu.execute(.lsr(.l, .r(.d1), .d0), length: 0)
+        
+        XCTAssertEqual(m.cpu.d0, 0b0010_1010_1010_1010_1010_1010_1010_1010)
+        XCTAssertEqual(m.cpu.ccr, [.x, .c])
+    }
+    
+    func testLsrLongRegZero() throws {
+        m.cpu.d0 = 0b0000_0000_0000_0000_0000_0000_1010_1010
+        m.cpu.d1 = 8
+        m.cpu.ccr = []
+        
+        m.cpu.execute(.lsr(.l, .r(.d1), .d0), length: 0)
+        
+        XCTAssertEqual(m.cpu.d0, 0)
+        XCTAssertEqual(m.cpu.ccr, [.x, .c, .z])
+    }
+    
+    func testLsrLongRegShiftCountZero() throws {
+        m.cpu.d0 = 0b0110_1100_0101_0101_0110_1100_0101_0101
+        m.cpu.d1 = 0
+        m.cpu.ccr = [.x, .c]
+        
+        m.cpu.execute(.lsr(.l, .r(.d1), .d0), length: 0)
+
+        XCTAssertEqual(m.cpu.d0, 0b0110_1100_0101_0101_0110_1100_0101_0101)
+        XCTAssertEqual(m.cpu.ccr, [.x])
+    }
 }
